7d37326617fc1591e02ffe5652474dcd0f0bde4b
Pavel Novik <brn2befree@gmail.com>
2024-02-28 17:32:13 +0300

    remove lazyGetData()

--- src/pages/decks-pages.tsx
+++ src/pages/decks-pages.tsx
@@ -14,9 +14,9 @@ import { TextField } from '@/components/ui/text-field/text-field'
 import {
   useCreateDeckMutation,
   useDeleteDeckMutation,
-  // useGetDecksQuery,
+  useGetDecksQuery,
   useGetMinMaxCardsQuery,
-  useLazyGetDecksQuery,
+  // useLazyGetDecksQuery,
 } from '@/services/base-api'
 
 export const DecksPages = () => {
@@ -24,15 +24,15 @@ export const DecksPages = () => {
   const { data: minMaxCardData } = useGetMinMaxCardsQuery()
 
   console.log(minMaxCardData)
-  // const { data, isError, isLoading } = useGetDecksQuery(
-  //   {
-  //     // maxCardsCount: minMaxCardData?.max,
-  //     // minCardsCount: minMaxCardData?.min,
-  //     name: search,
-  //   }
-  //   // { skip: !minMaxCardData }
-  // )
-  const [fetchData, { data, isError, isLoading }] = useLazyGetDecksQuery()
+  const { data, isError, isLoading } = useGetDecksQuery(
+    {
+      // maxCardsCount: minMaxCardData?.max,
+      // minCardsCount: minMaxCardData?.min,
+      name: search,
+    }
+    // { skip: !minMaxCardData }
+  )
+  // const [fetchData, { data, isError, isLoading }] = useLazyGetDecksQuery()
   const [createDeck, { isLoading: isDeckBeingCreated }] = useCreateDeckMutation()
   const [deleteDeck, { isLoading: isDeckBeingDeleted }] = useDeleteDeckMutation()
 
@@ -79,7 +79,7 @@ export const DecksPages = () => {
           ))}
         </TableBody>
       </Table>
-      <Button onClick={() => fetchData({ name: search })}>Fetch Data</Button>
+      {/*<Button onClick={() => fetchData({ name: search })}>Fetch Data</Button>*/}
     </Container>
   )
 }
