23b06361412a863e23fc080098fd1580ec3ac315
Pavel Novik <brn2befree@gmail.com>
2024-03-01 23:11:48 +0300

    change login page and import

--- src/components/auth/auth.service.ts
+++ src/components/auth/auth.service.ts
@@ -1,11 +1,23 @@
 import { baseApi } from '@/services/base-api'
 
+type LoginArgs = {
+  email: string
+  password: string
+  rememberMe: boolean
+}
 export const authService = baseApi.injectEndpoints({
   endpoints: builder => {
     return {
       getMe: builder.query<any, void>({
         query: () => ({
           url: `v1/auth/me`,
+        }),
+      }),
+      login: builder.mutation<void, LoginArgs>({
+        query: data => ({
+          body: data,
+          method: 'POST',
+          url: `v1/auth/login`,
         }),
       }),
     }
