fb6025973afba8e95a28113698ee2700e9d4e5c3
Pavel Novik <brn2befree@gmail.com>
2024-03-01 19:59:16 +0300

    change baseApi

--- src/services/base-api.ts
+++ src/services/base-api.ts
@@ -1,4 +1,3 @@
-import { CreateDeckArgs, Deck, Decks, GetDecksArgs } from '@/services/decks/decks.types'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const baseApi = createApi({
@@ -9,45 +8,7 @@ export const baseApi = createApi({
       headers.append('x-auth-skip', 'true')
     },
   }),
-  endpoints: builder => {
-    return {
-      createDeck: builder.mutation<Deck, CreateDeckArgs>({
-        invalidatesTags: ['Decks'],
-        query: body => ({
-          body,
-          method: 'POST',
-          url: `v1/decks`,
-        }),
-      }),
-      deleteDeck: builder.mutation<Deck, string>({
-        invalidatesTags: ['Decks'],
-        query: id => ({
-          method: 'DELETE',
-          url: `v1/decks/${id}`,
-        }),
-      }),
-      getDecks: builder.query<Decks, GetDecksArgs | void>({
-        providesTags: ['Decks'],
-        query: args => ({
-          params: args ?? undefined,
-          url: `v2/decks`,
-        }),
-      }),
-      getMinMaxCards: builder.query<any, void>({
-        query: () => ({
-          url: `v2/decks/min-max-cards`,
-        }),
-      }),
-    }
-  },
+  endpoints: () => ({}),
   reducerPath: 'baseApi',
   tagTypes: ['Decks'],
 })
-
-export const {
-  useCreateDeckMutation,
-  useDeleteDeckMutation,
-  useGetDecksQuery,
-  useGetMinMaxCardsQuery,
-  useLazyGetDecksQuery,
-} = baseApi
