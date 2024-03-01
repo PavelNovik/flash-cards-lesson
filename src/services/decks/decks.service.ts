d7e03feae0adb29d83509ef490df6897dfabdc22
Pavel Novik <brn2befree@gmail.com>
2024-03-01 19:59:05 +0300

    add deck service with baseApi

---
+++ src/services/decks/decks.service.ts
@@ -0,0 +1,44 @@
+import { baseApi } from '@/services/base-api'
+import { CreateDeckArgs, Deck, Decks, GetDecksArgs } from '@/services/decks/decks.types'
+
+export const decksService = baseApi.injectEndpoints({
+  endpoints: builder => {
+    return {
+      createDeck: builder.mutation<Deck, CreateDeckArgs>({
+        invalidatesTags: ['Decks'],
+        query: body => ({
+          body,
+          method: 'POST',
+          url: `v1/decks`,
+        }),
+      }),
+      deleteDeck: builder.mutation<Deck, string>({
+        invalidatesTags: ['Decks'],
+        query: id => ({
+          method: 'DELETE',
+          url: `v1/decks/${id}`,
+        }),
+      }),
+      getDecks: builder.query<Decks, GetDecksArgs | void>({
+        providesTags: ['Decks'],
+        query: args => ({
+          params: args ?? undefined,
+          url: `v2/decks`,
+        }),
+      }),
+      getMinMaxCards: builder.query<any, void>({
+        query: () => ({
+          url: `v2/decks/min-max-cards`,
+        }),
+      }),
+    }
+  },
+})
+
+export const {
+  useCreateDeckMutation,
+  useDeleteDeckMutation,
+  useGetDecksQuery,
+  useGetMinMaxCardsQuery,
+  useLazyGetDecksQuery,
+} = decksService
