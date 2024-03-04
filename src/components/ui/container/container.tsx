b14a856a6b81df87b5769936b92e1b054fa1995d
Pavel Novik <brn2befree@gmail.com>
2024-03-05 00:55:57 +0300

    change className and styles

--- src/components/ui/container/container.tsx
+++ src/components/ui/container/container.tsx
@@ -1,23 +1,13 @@
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
 import clsx from 'clsx'
-type Properties = {
-  [key: string]: string
-}
 
-const containerStyles: Properties = {
-  alignItems: 'center',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  margin: '50px auto',
-  maxWidth: '968px',
-}
+import s from './container.module.scss'
 
 export const Container = forwardRef<ElementRef<'div'>, ComponentPropsWithoutRef<'div'>>(
   ({ className, ...rest }, ref) => {
-    const classNames = clsx(className)
+    const classNames = clsx(s.container, className)
 
-    return <div className={classNames} ref={ref} style={containerStyles} {...rest} />
+    return <div className={classNames} ref={ref} {...rest} />
   }
 )
