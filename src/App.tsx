931f4ef64cd02152aee1e3e9bc66770a56b211e0
Pavel Novik <brn2befree@gmail.com>
2024-03-04 13:16:31 +0300

    remove unused comments

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,6 @@ import { Router } from '@/utils/router'
 
 export function App() {
   return (
-    // <div>
-    //   <br />
-    //   <Button as={'a'} href={'google.com'} variant={'primary'}>
-    //     hello
-    //   </Button>
-    //   <br />
-    //   <Button variant={'primary'}>hello</Button>
-    // </div>
     <Provider store={store}>
       <Router />
     </Provider>
