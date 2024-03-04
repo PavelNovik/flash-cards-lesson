c9bc52424b0c226aaf1cf3f80972ec5b5c6a4440
Pavel Novik <brn2befree@gmail.com>
2024-03-05 00:56:54 +0300

    create pagination

---
+++ src/components/ui/pagination/pagination.tsx
@@ -0,0 +1,30 @@
+import { Button } from '@/components/ui/button'
+
+export type PaginationProps = {
+  currentPage: number
+  onPageChange: (page: number) => void
+  onPageSizeChange: (pageSize: number) => void
+  pageSize: number
+}
+
+export const Pagination = ({
+  currentPage,
+  onPageChange,
+  onPageSizeChange,
+  pageSize,
+}: PaginationProps) => {
+  return (
+    <div style={{ display: 'flex', gap: '10px', marginTop: '50px' }}>
+      <Button disabled={currentPage <= 1} onClick={() => onPageChange(currentPage - 1)}>
+        Previous page
+      </Button>
+      <select name={'pages'} onChange={e => onPageSizeChange(+e.currentTarget.value)}>
+        <option value={5}>5</option>
+        <option value={7}>7</option>
+        <option value={10}>10</option>
+      </select>
+      {pageSize}
+      <Button onClick={() => onPageChange(currentPage + 1)}>Next page</Button>
+    </div>
+  )
+}
