8f6e52466ee657bd1044769496d15be91d0adfca
Pavel Novik <brn2befree@gmail.com>
2024-03-04 13:17:17 +0300

    add storybook import for browser router

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,10 +2,17 @@ import type { Preview } from '@storybook/react'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import '../src/styles/index.scss'
+import { reactRouterParameters, withRouter } from 'storybook-addon-react-router-v6'
 
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
+    reactRouter: reactRouterParameters({
+      routing: {
+        handle: 'Nav',
+        path: '*',
+      },
+    }),
     controls: {
       matchers: {
         color: /(background|color)$/i,
@@ -14,5 +21,6 @@ const preview: Preview = {
     },
   },
 }
+export const decorators = [withRouter]
 
 export default preview
